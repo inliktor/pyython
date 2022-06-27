b573ee4f8035a99593fd80f124eb41a3181998a5
ZWLoader <sandansorvi@gmail.com>
2022-06-27 15:12:41 +0400

    парсер гита надо сделать

--- парсер гита.py
+++ парсер гита.py
@@ -25,19 +25,34 @@ option.set_preference('media.volune_scale', '0.0')
 sel=webdriver.Firefox(options=option)
 try:
     def getddglinks(qtext):
-        links = []
         sel.get('https://duckduckgo.com/?q=' + qtext.replace(' ', '+'))
         elements = sel.find_elements_by_css_selector('.nrn-react-div')   
         xpath = '/html/body/div[2]/div[5]/div[3]/div/div[1]/div[1]/div/div[3]/a'
         button = sel.find_element_by_xpath(xpath).click()
-        xpath = '/html/body/div[7]/div[2]/div/div[1]/ol/li[3]/a'
+        xpath = '/html/body/div[2]/div[5]/div[3]/div/div[1]/div[5]/div[1]/article/div[2]/h2/a/span'
         button = sel.find_element_by_xpath(xpath).click()
+    links=getddglinks('https://github.com/inliktor/pyython')
+    for link in links:
+        print(link)
+    links = []
+    k = 1
+    elements = sel.find_elements_by_css_selector('.div.Box-row:nth-child()')
+    k=len(elements)
+    if k>0:
+        flag = 0
         for element in elements:
-            links.append(element.get_attribute('href'))
-        return links
-        links=getddglinks(input("Введите запрос: "))
-        for link in links:
-            print(link)
+            z=element.get_attribute("a")
+            if (z != none):
+                flag = 1
+                if z not in oldlinks:
+                    links.append(z)
+                    oldlinks.append(z)
+                    print(z)
+
+        
+
+
+
 
 
 
@@ -56,8 +71,10 @@ except:
 
 
 
-except:
-    pass 
+
+
+
+
 
 
     
